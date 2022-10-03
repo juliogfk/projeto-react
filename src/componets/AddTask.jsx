e9376402e10fb877e88dd08677799d8f6d6feb51
juliogfk <juliogfk1@gmail.com>
2022-10-03 14:08:41 -0300

    onchange do input criado

--- src/componets/AddTask.jsx
+++ src/componets/AddTask.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './AddTask.css';
 import Button from './Button';
 
 const AddTask = () => {
+    const [inputData, setInputData] = useState('')
+
+    const handleInputChange = (e) => {
+        setInputData(e.target.value);
+    };
     return (
         <div className='add-task-container'>
-            <input className='add-task-input' type="text" />;
+            <input className='add-task-input' onChange = {handleInputChange} value = {inputData} type="text" />
             <div className="add-task-button-container">
                 <Button>Adicionar</Button>
             </div>
